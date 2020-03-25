0eb6407cac45c2bb53d5d6b916172d9013877dd9
Ian Harcourt-Smith <ianearnest@hotmail.co.uk>
2020-03-25 22:37:48 +0100

    buttons functions

--- T2WindowsFormsApp/T2WindowsFormsApp/Form1.Designer.cs
+++ T2WindowsFormsApp/T2WindowsFormsApp/Form1.Designer.cs
@@ -48,7 +48,6 @@
             this.button15 = new System.Windows.Forms.Button();
             this.button16 = new System.Windows.Forms.Button();
             this.button17 = new System.Windows.Forms.Button();
-            this.btn01 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOne
@@ -59,7 +58,7 @@
             this.btnOne.TabIndex = 0;
             this.btnOne.Text = "1";
             this.btnOne.UseVisualStyleBackColor = true;
-            this.btnOne.Click += new System.EventHandler(this.button1_Click);
+            this.btnOne.Click += new System.EventHandler(this.btn1_Click);
             // 
             // label1
             // 
@@ -93,6 +92,7 @@
             this.btnTwo.TabIndex = 5;
             this.btnTwo.Text = "2";
             this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.btn2_Click);
             // 
             // button3
             // 
@@ -102,6 +102,7 @@
             this.button3.TabIndex = 6;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btn3_Click);
             // 
             // button4
             // 
@@ -111,6 +112,7 @@
             this.button4.TabIndex = 9;
             this.button4.Text = "6";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btn6_Click);
             // 
             // button5
             // 
@@ -120,6 +122,7 @@
             this.button5.TabIndex = 8;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btn5_Click);
             // 
             // button6
             // 
@@ -129,6 +132,7 @@
             this.button6.TabIndex = 7;
             this.button6.Text = "4";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.btn4_Click);
             // 
             // button7
             // 
@@ -138,6 +142,7 @@
             this.button7.TabIndex = 12;
             this.button7.Text = "9";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.btn9_Click);
             // 
             // button8
             // 
@@ -147,6 +152,7 @@
             this.button8.TabIndex = 11;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.btn8_Click);
             // 
             // button9
             // 
@@ -156,6 +162,7 @@
             this.button9.TabIndex = 10;
             this.button9.Text = "7";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.btn7_Click);
             // 
             // button10
             // 
@@ -165,6 +172,7 @@
             this.button10.TabIndex = 15;
             this.button10.Text = "Clear";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // button11
             // 
@@ -174,6 +182,7 @@
             this.button11.TabIndex = 14;
             this.button11.Text = ".";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.btnDOT_Click);
             // 
             // button12
             // 
@@ -183,6 +192,7 @@
             this.button12.TabIndex = 13;
             this.button12.Text = "0";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.btn0_Click);
             // 
             // button13
             // 
@@ -192,6 +202,7 @@
             this.button13.TabIndex = 19;
             this.button13.Text = "/";
             this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.btnDivide_Click);
             // 
             // button14
             // 
@@ -201,6 +212,7 @@
             this.button14.TabIndex = 18;
             this.button14.Text = "*";
             this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.btnMultiply_Click);
             // 
             // button15
             // 
@@ -210,6 +222,7 @@
             this.button15.TabIndex = 17;
             this.button15.Text = "-";
             this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.btnMinus_Click);
             // 
             // button16
             // 
@@ -219,6 +232,7 @@
             this.button16.TabIndex = 16;
             this.button16.Text = "+";
             this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.btnPlus_Click);
             // 
             // button17
             // 
@@ -228,24 +242,13 @@
             this.button17.TabIndex = 20;
             this.button17.Text = "=";
             this.button17.UseVisualStyleBackColor = true;
-            // 
-            // btn01
-            // 
-            this.btn01.Location = new System.Drawing.Point(586, 121);
-            this.btn01.Name = "btn01";
-            this.btn01.Size = new System.Drawing.Size(75, 23);
-            this.btn01.TabIndex = 21;
-            this.btn01.Text = "btn00";
-            this.btn01.TextAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.btn01.UseVisualStyleBackColor = true;
-            this.btn01.Click += new System.EventHandler(this.button1_Click_1);
+            this.button17.Click += new System.EventHandler(this.btnEquals_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btn01);
             this.Controls.Add(this.button17);
             this.Controls.Add(this.button13);
             this.Controls.Add(this.button14);
@@ -296,7 +299,6 @@
         private System.Windows.Forms.Button button15;
         private System.Windows.Forms.Button button16;
         private System.Windows.Forms.Button button17;
-        private System.Windows.Forms.Button btn01;
     }
 }
 
--- T2WindowsFormsApp/T2WindowsFormsApp/Form1.cs
+++ T2WindowsFormsApp/T2WindowsFormsApp/Form1.cs
@@ -21,28 +21,101 @@ namespace T2WindowsFormsApp
         //TODO
         // Calc https://www.homeandlearn.co.uk/csharp/csharp_s2p18.html
         // Menus? https://www.homeandlearn.co.uk/csharp/csharp_s4p1.html
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click1(object sender, EventArgs e)
         {
-            string input;
-            input = textBox1.Text;
-            MessageBox.Show(input);
-            //MessageBox.Show("Hi", "Title", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            
         }
 
-        private void btnTwo_Click(object sender, EventArgs e)
-        {
-            string input;
-            input = textBox1.Text;
-            MessageBox.Show(input);
-            //MessageBox.Show("Hi", "Title", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //string input;
+            //input = textBox1.Text;
+            //MessageBox.Show(input);
+            //MessageBox.Show("Hi", "Title", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDOT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
         {
 
         }
